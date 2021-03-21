5fad79e530dd3047742608a718aa18be6373360f
Şerife Benli <1030520687@erciyes.edu.tr>
2021-03-22 01:18:19 +0300

    Add project files.

---
+++ Entities/Concrete/Category.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //çıplak class kalmasın 
+    //bu sınıflar veritabanında tablo isimlerin denk geliyor
+    //işaretleme yapalım
+
+   public class Category:IEntity
+    {
+        public int CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
